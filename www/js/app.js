5ca59919bbd2d81a97f7e19f2ef41b9298c7f39b
jstodd <jstodd2612@gmail.com>
2015-10-13 22:32:27 -0600

    Login and added Boostrap
    Changed the Login page and added bootstrap to the code

--- www/js/app.js
+++ www/js/app.js
@@ -32,11 +32,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
+  .state('app.home', {
+    url: '/home',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/home.html'
       }
     }
   })
@@ -70,5 +70,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/home');
 });
